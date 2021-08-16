dfcfde82eeb6ba3b47550d68c5f666e66bba83ed
101010sun <D0782838@mail.fcu.edu.tw>
2021-08-16 22:47:03 +0800

    整理函式

--- BlockOperation.py
+++ BlockOperation.py
@@ -2,38 +2,6 @@ import hashlib
 import Format
 import time
 
-# 將交易明細轉換成字串
-def transaction_to_string(self,transaction): #transaction to string
-    transaction_dict = {
-        'sender': str(transaction.sender),
-        'receiver': str(transaction.sender),
-        'amounts': transaction.amounts,
-        'fee': transaction.fee,
-        'message': transaction.message
-    }
-    return str(transaction_dict)
-
-# 把區塊紀錄內的所有交易明細轉換成一個字串
-def get_transaction_string(self,block): #take all transaction from block turn to string 
-    transaction_str = ''
-    for transaction in block.transactions:
-        transaction_str += self.transaction_to_string(transaction) #transaction_str = transaction_str + self.transaction_to_string(transaction)
-    return transaction_str 
-
-# 依據資料產生相對應的雜湊值
-def get_hash(self,block,nonce):
-    s = hashlib.sha256()
-    s.update(
-        (
-            block.previous_hash 
-            + str(block.timestamp) #When block produce
-            + str.get_transaction_string(block) #all transaction from block
-            + str(nonce) #mining nonce
-        ).encode("utf-8")
-    ) #Update hash SHA256
-    h = s.hexdigest() #get hash
-    return h
-
 # 產生創世塊
 def create_genesis_block(self):
     print("Create genesis block...")
--- Format.py
+++ Format.py
@@ -1,4 +1,5 @@
 import time
+import hashlib
 
 class Transaction: 
     def __init__(self,sender,receiver,amounts,fee,message,community):
@@ -8,6 +9,7 @@ class Transaction:
         self.fee = fee 
         self.message = message 
         self.community = community
+
 
     #  # test method
     # def test(self):
@@ -21,7 +23,41 @@ class Block:
         self.nonce = 0 #key
         self.timestamp = int(time.time()) 
         self.transactions = [] 
-        self.node = node 
+        self.node = node
+
+    # 將交易明細轉換成字串(dict)
+    def transaction_to_string(self, transaction): #transaction to string
+        transaction_dict = {
+            'sender': str(transaction.sender),
+            'receiver': str(transaction.sender),
+            'amounts': transaction.amounts,
+            'fee': transaction.fee,
+            'message': transaction.message
+        }
+        return str(transaction_dict)
+
+    # 把區塊紀錄內的所有交易明細轉換成一個字串
+    def get_transaction_string(self): #take all transaction from block turn to string 
+        transaction_str = ''
+        for transaction in self.transactions:
+            transaction_str += self.transaction_to_string(transaction) #transaction_str = transaction_str + self.transaction_to_string(transaction)
+        return transaction_str 
+
+    # 依據資料產生相對應的雜湊值
+    def get_hash(self):
+        s = hashlib.sha256()
+        s.update(
+            (
+                self.previous_hash 
+                + str(self.timestamp) #When block produce
+                + str(self.get_transaction_string()) #all transaction from block
+                + str(self.nonce) #mining nonce
+            ).encode("utf-8")
+        ) #Update hash SHA256
+        h = s.hexdigest() #get hash
+        return h
+
+
 
 class BlockChain: 
     def __init__(self):
