22b9ead3137d064dcdf0691a178ce410f8dcef9f
D0782868 <samantha890809@gmail.com>
2021-10-28 23:36:38 +0800

    驗證

--- Blockchain.py
+++ Blockchain.py
@@ -264,6 +264,31 @@ class BlockChain:
             return True
         except Exception:
             print("RSA Verified wrong!")
+            
+    #驗證區塊
+    def receive_broadcast_block(self, block_data):
+        last_block = self.chain[-1]
+        # Check the hash of received block
+        if block_data.previous_hash != last_block.hash:
+            print("[**] Received block error: Previous hash not matched!")
+            return False
+        elif block_data.difficulty != self.difficulty:
+            print("[**] Received block error: Difficulty not matched!")
+            return False
+        elif block_data.hash != self.get_hash(block_data, block_data.nonce):
+            print(block_data.hash)
+            print("[**] Received block error: Hash calculation not matched!")
+            return False
+        else:
+            if block_data.hash[0: self.difficulty] == '0' * self.difficulty:
+                for transaction in block_data.transactions:
+                    self.chain.remove(transaction)
+                self.receive_verified_block = True
+                self.chain.append(block_data)
+                return True
+            else:
+                print(f"[**] Received block error: Hash not matched by diff!")
+            return False
 
       ###Wallet      
     # 產生錢包地址
